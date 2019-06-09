88ba7e96a286c0f273ee4b7a758f96d69afaafa4
hasherezade <hasherezade@gmail.com>
2019-06-09 16:40:39 +0200

    [BUGFIX][run_me.bat] Fixed invalid traced module set

--- install32_64/run_me.bat
+++ install32_64/run_me.bat
@@ -14,7 +14,7 @@ set PINTOOL=%PINTOOL32%
 
 set TARGET_APP=%1%
 rem TRACED_APP - by default it is the main module, but it can be also a DLL within the traced process
-set TRACED_APP=%1%
+set TRACED_APP=%TARGET_APP%
 set ENABLE_SHORT_LOGGING=1
 
 %PIN_TOOLS_DIR%\pe_check.exe %TARGET_APP%
@@ -27,7 +27,7 @@ if %errorlevel% == 64 (
 	set PINTOOL=%PINTOOL64%
 )
 
-%PIN_DIR%\pin.exe -t %PINTOOL% -m %TARGET_APP% -o %TARGET_APP%.tag -s %ENABLE_SHORT_LOGGING% -- %TARGET_APP% 
+%PIN_DIR%\pin.exe -t %PINTOOL% -m %TRACED_APP% -o %TARGET_APP%.tag -s %ENABLE_SHORT_LOGGING% -- %TARGET_APP% 
 
 if %ERRORLEVEL% EQU 0 echo [OK] PIN tracing finished: the traced application terminated.
 rem Pausing script after the application is executed is useful to see all eventual printed messages and for troubleshooting
