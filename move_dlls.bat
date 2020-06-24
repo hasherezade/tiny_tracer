48e399596d780321a4037bc4ee50c479842a5c74
hasherezade <hasherezade@gmail.com>
2020-06-24 20:56:22 +0200

    [REFACT] Modified move_dlls.bat

--- move_dlls.bat
+++ move_dlls.bat
@@ -1,4 +1,5 @@
-rem First, compile the 32 and 64 bit version of TinyTracer. Then, you can use this script to copy them into the directory with the run_me.bat (default: install32_64).
-move Release\TinyTracer.dll install32_64\TinyTracer32.dll
-move x64\Release\TinyTracer.dll install32_64\TinyTracer64.dll
+rem First, compile the 32 and 64 bit version of TinyTracer (in a Release mode). Then, you can use this script to copy them into the directory with the run_me.bat (default: install32_64).
+set INSTALL_DIR=install32_64
+move Release\TinyTracer.dll %INSTALL_DIR%\TinyTracer32.dll
+move x64\Release\TinyTracer.dll %INSTALL_DIR%\TinyTracer64.dll
 pause
