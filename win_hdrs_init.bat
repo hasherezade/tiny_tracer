4f6e0d972b125365cc0a7488265e595b8ef4fc1f
hasherezade <hasherezade@gmail.com>
2023-08-09 19:36:02 +0200

    [FEATURE] Added a script to autogenerate my_paths.h, use it as a pre-build event

---
+++ win_hdrs_init.bat
@@ -0,0 +1,20 @@
+setlocal enabledelayedexpansion
+
+set search_dir="C:\Program Files (x86)\Windows Kits\"
+set output_file="my_paths.h"
+set header_name="Windows.h"
+
+for /r "C:\Program Files (x86)\Windows Kits\" %%i in (*%header_name%) do (
+    set "sdir=%%~dpi"
+	echo %%~dpi
+)
+
+if defined sdir (
+	echo|(set /p="#define _WINDOWS_H_PATH_ %sdir:\=/%") > %output_file%
+    echo Header subdirectory path found and saved to %output_file%.
+) else (
+    echo Header path not found.
+)
+endlocal
+goto :eof
+
