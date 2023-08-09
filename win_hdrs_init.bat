b28a9de47fa642891c678b3c902f09bb5d97f193
hasherezade <hasherezade@gmail.com>
2023-08-09 11:03:04 -0700

    [REFACT] Cleanup in win_hdrs_init.bat

--- win_hdrs_init.bat
+++ win_hdrs_init.bat
@@ -5,17 +5,15 @@ set search_dir="C:\Program Files (x86)\Windows Kits\"
 set output_file="my_paths.h"
 set header_name="Windows.h"
 
-for /r "C:\Program Files (x86)\Windows Kits\" %%i in (*%header_name%) do (
+for /r %search_dir% %%i in (*%header_name%) do (
     set "sdir=%%~dpi"
-	echo %%~dpi
 )
 
 if defined sdir (
-	echo|(set /p="#define _WINDOWS_H_PATH_ %sdir:\=/%") > %output_file%
+    echo|(set /p="#define _WINDOWS_H_PATH_ %sdir:\=/%") > %output_file%
     echo Header subdirectory path found and saved to %output_file%.
 ) else (
     echo Header path not found.
 )
 endlocal
 goto :eof
-
