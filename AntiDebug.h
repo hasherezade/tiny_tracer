2fe7e6d328402f766fd966634626b28fe8198916
Andrea Naspi <andreanaspi@gmail.com>
2023-08-17 23:09:39 +0200

    [FEATURE] AntiDebug: bypass VMProtect anti-debug check based on Trap Flag

--- AntiDebug.h
+++ AntiDebug.h
@@ -10,5 +10,6 @@ namespace AntiDbg {
 	VOID WatchThreadStart(THREADID threadid, CONTEXT* ctxt, INT32 flags, VOID* v);
 	VOID MonitorAntiDbgFunctions(IMG Image);
 	VOID FlagsCheck(const CONTEXT* ctxt);
+	VOID FlagsCheck_after(CONTEXT* ctx, THREADID tid, ADDRINT eip, ADDRINT esp);
 	VOID InterruptCheck(const CONTEXT* ctxt);
 };
