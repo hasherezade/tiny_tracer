d693435ddbb7e9c7cdf99aa141078c35706f5c9e
hasherezade <hasherezade@gmail.com>
2024-12-01 04:03:32 +0100

    [BUGFIX] Fixed building on Linux

--- make_linux.sh
+++ make_linux.sh
@@ -3,8 +3,8 @@
 make all TARGET=ia32
 make all TARGET=intel64
  
-TT_32=./obj-ia32/TinyTracer.so 
-TT_64=./obj-intel64/TinyTracer.so
+TT_32=./obj-ia32/TinyTracer32.so 
+TT_64=./obj-intel64/TinyTracer64.so
  
 APP_TYPE32=$(file "$TT_32")
 APP_TYPE64=$(file "$TT_64")
@@ -12,7 +12,7 @@ APP_TYPE64=$(file "$TT_64")
 if [[ $APP_TYPE64 == *"ELF 64-bit"* ]];
 then
     echo "[+] 64 bit build ok."
-    cp ./obj-intel64/TinyTracer.so ./install32_64/TinyTracer64.so
+    cp "$TT_64" ./install32_64/TinyTracer64.so
     if [[ $? == 0 ]];
     then
     	echo "[+] 64 bit install ok."
@@ -26,7 +26,7 @@ fi
 if [[ $APP_TYPE32 == *"ELF 32-bit"* ]];
 then
     echo "[+] 32 bit build ok."
-    cp ./obj-ia32/TinyTracer.so ./install32_64/TinyTracer32.so
+    cp "$TT_32" ./install32_64/TinyTracer32.so
     if [[ $? == 0 ]];
     then
     	echo "[+] 32 bit install ok."
