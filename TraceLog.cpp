85f58993ed9cfb79202775855fe844d04fa47b44
hasherezade <hasherezade@gmail.com>
2026-05-24 08:18:29 -0800

    [REFACT] In TraceLog: use logLine in every other logging function

--- TraceLog.cpp
+++ TraceLog.cpp
@@ -2,56 +2,56 @@
 
 #include "Util.h"
 
+#include <iostream>
+#include <sstream>
+
 void TraceLog::logCall(const ADDRINT prevModuleBase, const ADDRINT prevAddr, bool isRVA, const std::string &module, const std::string &func)
 {
-    if (!createFile()) return;
+    std::stringstream ss;
     ADDRINT rva = (isRVA) ? prevAddr : prevAddr - prevModuleBase;
     if (!isRVA) {
-        m_traceFile << "> " << prevModuleBase << "+";
+        ss << "> " << prevModuleBase << "+";
     }
-    m_traceFile <<
+    ss <<
         std::hex << rva
         << DELIMITER;
 
     if (!m_shortLog) {
-        m_traceFile << "called: "
+        ss << "called: "
             << module;
     }
     else {
-        m_traceFile << util::getDllName(module);
+        ss << util::getDllName(module);
     }
     if (func.length() > 0) {
-        m_traceFile << "." << func;
+        ss << "." << func;
     }
-    m_traceFile << std::endl;
-    m_traceFile.flush();
+    logLine(ss.str());
 }
 
 void TraceLog::logCall(const ADDRINT prevBase, const ADDRINT prevAddr, const ADDRINT calledPageBase, const ADDRINT callAddr)
 {
-    if (!createFile()) return;
+    std::stringstream ss;
     if (prevBase) {
-        m_traceFile << "> " << prevBase << "+";
+        ss << "> " << prevBase << "+";
     }
     const ADDRINT rva = callAddr - calledPageBase;
-    m_traceFile << 
-        std::hex << prevAddr 
-        << DELIMITER 
-        << "called: ?? [" << calledPageBase << "+" << rva << "]"
-        << std::endl;
-    m_traceFile.flush();
+    ss <<
+        std::hex << prevAddr
+        << DELIMITER
+        << "called: ?? [" << calledPageBase << "+" << rva << "]";
+    logLine(ss.str());
 }
 
 void TraceLog::logCallRet(const ADDRINT prevBase, const ADDRINT prevAddr, const ADDRINT retPageBase, const ADDRINT retAddr, const std::string &module, const std::string &func)
 {
-    if (!createFile()) return;
-
+    std::stringstream ss;
     ADDRINT retRva = retAddr;
     if (retPageBase) {
         retRva -= retPageBase;
-        m_traceFile << "> " << retPageBase << "+";
+        ss << "> " << retPageBase << "+";
     }
-    m_traceFile
+    ss
         << std::hex << retRva
         << DELIMITER
         << "RET from: "
@@ -59,107 +59,98 @@ void TraceLog::logCallRet(const ADDRINT prevBase, const ADDRINT prevAddr, const 
         << util::getDllName(module);
 
     if (func.length() > 0) {
-        m_traceFile << "." << func;
+        ss << "." << func;
     }
-    m_traceFile << std::endl;
-    m_traceFile.flush();
+    logLine(ss.str());
 }
 
 
 void TraceLog::logSectionChange(const ADDRINT prevAddr, std::string &name)
 {
-    if (!createFile()) return;
-    m_traceFile 
+    std::stringstream ss;
+    ss
         << std::hex << prevAddr 
         << DELIMITER 
         << "section: [" << name << "]"
         << std::endl;
-    m_traceFile.flush();
+    logLine(ss.str());
 }
 
 void TraceLog::logIndirectCall(const ADDRINT prevModuleBase, const ADDRINT prevAddr, bool isRVA, const ADDRINT calledBase, const ADDRINT calledRVA, const char* disasm)
 {
-    if (!createFile()) return;
+    std::stringstream ss;
     ADDRINT rva = (isRVA) ? prevAddr : prevAddr - prevModuleBase;
     if (!isRVA) {
-        m_traceFile << "> " << prevModuleBase << "+";
+        ss << "> " << prevModuleBase << "+";
     }
-    m_traceFile <<
-        std::hex << rva
+    ss
+        << std::hex << rva
         << DELIMITER;
     if (disasm) {
-        m_traceFile << "[" << disasm << "] ";
+        ss << "[" << disasm << "] ";
     }
-    m_traceFile
-        << "to: " << (calledBase + calledRVA) << " [" << calledBase << " + " << calledRVA << "]"
-        << std::endl;
-    m_traceFile.flush();
+    ss << "to: " << (calledBase + calledRVA) << " [" << calledBase << " + " << calledRVA << "]";
+    logLine(ss.str());
 }
 
 void TraceLog::logInstruction(const ADDRINT base, const ADDRINT rva, const std::string& mnem, const ADDRINT param)
 {
-    if (!createFile()) return;
+    std::stringstream ss;
     if (base) {
-        m_traceFile << "> " << std::hex << base << "+";
+        ss << "> " << std::hex << base << "+";
     }
-    m_traceFile
+    ss
         << std::hex << rva
         << DELIMITER
         << mnem << ":"
-        << std::hex << param
-        << std::endl;
-    m_traceFile.flush();
+        << std::hex << param;
+    logLine(ss.str());
 }
 
 void TraceLog::logInstruction(const ADDRINT base, const ADDRINT rva, const std::string& mnem)
 {
-    if (!createFile()) return;
+    std::stringstream ss;
     if (base) {
-        m_traceFile << "> " << std::hex << base << "+";
+        ss << "> " << std::hex << base << "+";
     }
-    m_traceFile
+    ss
         << std::hex << rva
         << DELIMITER
-        << mnem
-        << std::endl;
-    m_traceFile.flush();
+        << mnem;
+    logLine(ss.str());
 }
 
 void TraceLog::logSyscall(const ADDRINT base, const ADDRINT rva, const ADDRINT param, const std::string& funcName)
 {
-    if (!createFile()) return;
+    std::stringstream ss;
     if (base) {
-        m_traceFile << "> " << std::hex << base << "+";
+        ss << "> " << std::hex << base << "+";
     }
-    m_traceFile
+    ss
         << std::hex << rva
         << DELIMITER
         << "SYSCALL:0x"
         << std::hex << param;
     if (!funcName.empty()) {
-        m_traceFile << "(" << funcName << ")";
+        ss << "(" << funcName << ")";
     }
-    m_traceFile << std::endl;
-    m_traceFile.flush();
+    logLine(ss.str());
 }
 
-void TraceLog::logLine(const std::string &str)
+void TraceLog::logLine(const std::string& str)
 {
     if (!createFile()) return;
 
-    m_traceFile
-        << str
-        << std::endl;
+    m_traceFile << str << std::endl;
     m_traceFile.flush();
 }
 
 void TraceLog::logNewSectionCalled(const ADDRINT prevAddr, const std::string &prevSection, const std::string &currSection)
 {
-    createFile();
-    m_traceFile
+    std::stringstream ss;
+    ss
         << std::hex << prevAddr
         << DELIMITER
-        << "[" << prevSection << "] -> [" << currSection << "]"
-        << std::endl;
-    m_traceFile.flush();
+        << "[" << prevSection << "] -> [" << currSection << "]";
+    logLine(ss.str());
 }
